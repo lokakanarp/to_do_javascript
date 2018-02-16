6d4ede64c7070f6a986fac4e82538e4b407cd2a6
Loka Kanarp <lokakanarp@gmail.com>
2018-02-16 16:42:28 +0100

    Finding the index of object in array

--- main.js
+++ main.js
@@ -1,41 +1,79 @@
 const input = document.getElementById('input');
 const addButton = document.getElementById('addButton');
 const myToDoListUl = document.getElementById('my_to_do_list_ul');
+const removeButton = document.getElementById("removeButton");
 
-const arrayOfTasks = [];
+let arrayOfTasks = [];
 
-/*bookButton.addEventListener('click', function(){
-	books.push({titel: "katten och Ekorren", author: "Loka", isRead: true, url: "http://packetpushers.net/wp-content/uploads/2015/04/accessories-dictionary.png"});
-	console.log(books);
-	});*/
 
+//En funktion som skapar objekt och lägger i array:en och returnerar array:en.
 function putObjectInArray(array, task) {        
 	array.push({todo: task, done: false});						
 	return array;
 };
-//En funktion som skapar objekt och lägger i array:en och returnerar array:en.
+
 
 let toDoListHTML = ""; //En tom string till att börja med.
 
+//En funktion som lägger ihop info från en Array till en chunk med HTML.
 function makeHTMLOutOfArray(array, string) {
 	for(var i = 0; i < array.length; i++){
+		if(array[i].done == false) {
 	  string += `<li>${array[i].todo}</li>`;
+		}
 	};
 	return string;
 };
-//En funktion som lägger ihop info från en Array till en chunk med HTML.
+
+
+/*addButton.addEventListener('click', function(){
+	putObjectInArray(arrayOfTasks, input.value);
+	myToDoListUl.innerHTML = makeHTMLOutOfArray(arrayOfTasks, toDoListHTML);
+	
+});*/
+
+/*const listItems = document.querySelectorAll('li');
+
+for(const item of listItems){
+    item.addEventListener('click', function(){
+        this.parentElement.removeChild(this);
+    })
+}
+*/
 
 addButton.addEventListener('click', function(){
 	putObjectInArray(arrayOfTasks, input.value);
-	//console.log(arrayOfTasks);
 	myToDoListUl.innerHTML = makeHTMLOutOfArray(arrayOfTasks, toDoListHTML);
+	console.log(arrayOfTasks);
+	const listItems = document.querySelectorAll('li');
 	
-	//myToDoListUl.innerHTML = toDoListHTML;
+	
+/*function findObjectByValue(array, value) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].todo == value) {
+            return array[i];
+        }
+    }
+    return null;
+}*/
+	
+
+
+for(const item of listItems){
+    item.addEventListener('click', function(){
+        //this.parentElement.removeChild(this);
+		//console.log(this.textContent)
+		//console.log(arrayOfTasks.indexOf(this.textContent));
+		//let x =findObjectByValue(arrayOfTasks, this.textContent);
+		//console.log(x);
+		let index = arrayOfTasks.findIndex(x => x.todo == this.textContent);
+		console.log(index);
+		arrayOfTasks[index].done = true;
+		console.log(arrayOfTasks);
+    })
+	
+}
 });
-
-
-
-
 
 
 
